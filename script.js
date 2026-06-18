acbdc0905361d9557d057183686153acc07f678c
chaoticknives <205522400+chaoticknives@users.noreply.github.com>
2026-06-18 19:54:21 -0400

    Add humanScore and computerScore

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // console.log("Hello World"); <- testing
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice (choice) {
     let computerChoice = Math.floor(Math.random() * 3) + 1
     let computerResult
@@ -26,3 +29,5 @@ function getHumanChoice (choice) {
 
 console.log(getComputerChoice());
 console.log(getHumanChoice());
+console.log(humanScore);
+console.log(computerScore);
