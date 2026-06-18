c03ae71034ff537bcddead79b651522bbbd4482f
chaoticknives <205522400+chaoticknives@users.noreply.github.com>
2026-06-18 19:30:20 -0400

    Create getComputerChoice function
    Random number generator between 1-3, with each number being assigned to a different rock/paper/scissor choice. Returns random computer choice.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+// console.log("Hello World"); <- testing
+
+function getComputerChoice (choice) {
+    let computerChoice = Math.floor(Math.random() * 3) + 1
+    let computerResult
+
+    if (computerChoice === 1) {
+        computerResult = "rock";
+    }
+    else if (computerChoice === 2) {
+        computerResult = "paper";
+    } else if (computerChoice === 3) {
+        computerResult = "scissors";
+    }
+    else {
+        computerResult = "How did you mess up?";
+    }
+
+    return computerResult;
+}
+
+console.log(getComputerChoice());
